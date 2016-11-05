e02a7d33675f4b55180bcdbfebe1c17ab5b1285d
Paul Walker <paul.walker@redlion.net>
2016-11-05 10:49:47 -0500

    dd-wrt_nvram_backup.sh: added script to back up nvram from a remote machine using ssh

---
+++ scripts/dd-wrt_nvram_backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+usage() {
+cat <<EOF
+$(basename $0) <ip of dd-wrt router>
+EOF
+}
+
+case "$1" in
+	-h|--help|"")
+		usage
+		;;
+	*)
+		ssh "root@$1" "nvram show" | sort > "$(date +%Y-%m-%d)_$1.nvram"
+		;;
+esac
