b9e83921ac52744a567424d065009a915bbd523d
Paul Walker <wpaulsg@gmail.com>
2019-03-30 18:21:59 -0500

    .atom/git-pkg.sh: updated install to only install missing/outdated packages

--- .atom/git-pkg.sh
+++ .atom/git-pkg.sh
@@ -8,8 +8,15 @@ case "$1" in
         apm list --installed --bare > $atom_dir/package.list
         ;;
     "install")
-        set -x
-        apm install --packages-file $atom_dir/package.list
+        list="$(apm list --installed)"
+        cat $atom_dir/package.list | while read -r package; do
+            if ! echo "$list" | grep --quiet $package; then
+                echo apm install $package
+                     apm install $package
+            elif [ "$package" ]; then
+                echo Skipping $package
+            fi
+        done
         ;;
     *)
         echo "Usage: $0 {save|install}"
