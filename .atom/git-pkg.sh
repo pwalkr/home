bad006340d265e07657b64a9a8dbdba8c75c49f6
Paul Walker <wpaulsg@gmail.com>
2019-04-19 11:27:19 -0500

    .atom/git-pkg.sh: quote package

--- .atom/git-pkg.sh
+++ .atom/git-pkg.sh
@@ -10,11 +10,11 @@ case "$1" in
     "install")
         list="$(apm list --installed)"
         cat $atom_dir/package.list | while read -r package; do
-            if ! echo "$list" | grep --quiet $package; then
-                echo apm install $package
-                     apm install $package
+            if ! echo "$list" | grep --quiet "$package"; then
+                echo apm install "$package"
+                     apm install "$package"
             elif [ "$package" ]; then
-                echo Skipping $package
+                echo Skipping "$package"
             fi
         done
         ;;
