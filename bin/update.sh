9c340b5e5368f0f6e7af53881cad58f4a6bededf
Paul Walker <wpaulsg@gmail.com>
2024-10-19 22:41:35 -0400

    update.sh: call reflector before pacman

--- bin/update.sh
+++ bin/update.sh
@@ -2,6 +2,14 @@
 
 set -x
 
+sudo reflector \
+    --age 12 \
+    --country "US" \
+    --latest 5 \
+    --protocol https \
+    --sort rate \
+    --save /etc/pacman.d/mirrorlist
+
 sudo pacman -Syu
 
 arch-aur-install.sh brave-bin
